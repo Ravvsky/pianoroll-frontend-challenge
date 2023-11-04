a82bf40217c77a7fd2ee2dce0d94f905388e81a3
Ravvsky <pawel.rawsk@gmail.com>
2023-11-04 19:13:02 +0100

    feat: add resize functionality to overlay

--- piano/app/components/MainView.tsx
+++ piano/app/components/MainView.tsx
@@ -1,39 +1,87 @@
+"use client";
 import { useAppSelector } from "../redux/hooks";
-import Link from "next/link";
 import parseSvgElement from "../utils/parseSvgElement";
 import { redirect } from "next/navigation";
+import { MouseEvent, useRef, useState } from "react";
+import Overlay from "./Overlay";
 
 const MainView = ({ itemId }: { itemId: number }) => {
   const rollsList = useAppSelector((state) => state.rollsList.items);
+
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [distance, setDistance] = useState(0);
+  const [resizingHandle, setResizingHandle] = useState("");
+
+  const divRef = useRef(null);
+  const rightHandleRef = useRef<HTMLDivElement | null>(null);
+  const leftHandleRef = useRef<HTMLDivElement | null>(null);
+
   if (!rollsList || itemId === undefined) {
     redirect("/");
   }
   const mainRoll = parseSvgElement(rollsList[itemId].svgElement);
 
+  const handleMouseDown = (e: MouseEvent, side: string) => {
+    if (side === "left") {
+      setResizingHandle("left");
+      setIsMouseDown(true);
+    } else {
+      setResizingHandle("right");
+      setIsMouseDown(true);
+    }
+    setDistance(0);
+  };
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isMouseDown) {
+      return;
+    }
+    if (resizingHandle === "right") {
+      if (rightHandleRef.current) {
+        const newDistance =
+          e.clientX - rightHandleRef.current?.getBoundingClientRect().right;
+        setDistance(newDistance);
+      }
+    } else if (resizingHandle === "left") {
+      if (leftHandleRef.current) {
+        const newDistance =
+          e.clientX - leftHandleRef.current?.getBoundingClientRect().right;
+        setDistance(-newDistance);
+      }
+    }
+  };
+
   return (
-    <div className="flex flex-col md:grid grid-cols-12 gap-[2rem] container m-auto h-full">
-      <div className="flex flex-col container m-auto  md:col-span-9 lg:col-span-10 h-full">
-        Piano Roll number {itemId}
-        <div
-          className="h-[50vh] flex items-start"
-          dangerouslySetInnerHTML={{ __html: mainRoll.outerHTML }}
-        ></div>
-      </div>
-      <div className="md:col-span-3  lg:col-span-2 flex flex-col gap-[2rem] overflow-auto">
-        {rollsList.map((roll, index) => {
-          const svgElement = parseSvgElement(roll.svgElement);
-          return (
-            <Link
-              key={index}
-              href={{
-                pathname: "/main-view",
-                query: { id: index },
-              }}
-              className=" h-[20vh] md:h-[15vh]"
-              dangerouslySetInnerHTML={{ __html: svgElement.outerHTML }}
-            ></Link>
-          );
-        })}
+    <div className="container m-auto flex h-full  flex-col md:col-span-9 lg:col-span-10">
+      Piano Roll number {itemId}
+      <div ref={divRef} className="relative">
+        <div onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
+          <div
+            className="flex h-[50vh] items-start"
+            dangerouslySetInnerHTML={{ __html: mainRoll.outerHTML }}
+          ></div>
+
+          <Overlay
+            divRef={divRef}
+            newDistance={distance}
+            isResizing={isMouseDown}
+            resizingHandle={resizingHandle}
+          >
+            {" "}
+            <div
+              ref={rightHandleRef}
+              onMouseDown={(e) => handleMouseDown(e, "right")}
+              className=" h-full w-[1rem] cursor-col-resize  bg-[#00ff00]"
+            ></div>
+            <div
+              ref={leftHandleRef}
+              onMouseDown={(e) => handleMouseDown(e, "left")}
+              className=" h-full w-[1rem] cursor-col-resize  bg-[#00ffff]"
+            ></div>
+          </Overlay>
+        </div>
       </div>
     </div>
   );
