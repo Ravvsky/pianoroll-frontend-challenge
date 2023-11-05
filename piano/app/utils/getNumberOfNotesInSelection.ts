ffb7e584839e0ecef666532932fc395111cdef36
Ravvsky <pawel.rawsk@gmail.com>
2023-11-05 18:16:49 +0100

    feat: create getNumberofNotesInSelection function

---
+++ piano/app/utils/getNumberOfNotesInSelection.ts
@@ -0,0 +1,38 @@
+import { MutableRefObject } from "react";
+
+const getNumberOfNotesInSelection = (
+  notesSelector: string,
+  roll: Element,
+  ref: MutableRefObject<HTMLElement>,
+  overlayStartingPoint: number,
+  overlayWidth: number,
+) => {
+  if (!ref.current) {
+    return;
+  }
+  const notes = roll.querySelectorAll(notesSelector);
+  const startingPointInPercentage =
+    overlayStartingPoint / ref.current.offsetWidth;
+  const overlayEndingPoint = overlayStartingPoint + overlayWidth;
+
+  const endingPointInPercentage = overlayEndingPoint / ref.current.offsetWidth;
+
+  let count = 0;
+
+  notes.forEach((note) => {
+    const xValue = note.getAttribute("x");
+
+    if (xValue !== null) {
+      const parsedXValue = parseFloat(xValue);
+      if (
+        parsedXValue >= startingPointInPercentage &&
+        parsedXValue <= endingPointInPercentage
+      ) {
+        count++;
+      }
+    }
+  });
+
+  return count;
+};
+export default getNumberOfNotesInSelection;
